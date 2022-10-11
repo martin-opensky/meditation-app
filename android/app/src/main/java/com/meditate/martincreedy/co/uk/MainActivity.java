2b45651e02e419c50253819a28cecbdd2985b64f
Martin Creedy <martin@openskydevelopment.co.uk>
2022-10-11 09:25:20 +0100

    Mid-way through Capacitor JS

---
+++ android/app/src/main/java/com/meditate/martincreedy/co/uk/MainActivity.java
@@ -0,0 +1,5 @@
+package com.meditate.martincreedy.co.uk;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.meditate.martincreedy.co.uk',
+  appName: 'Simply Meditate',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
